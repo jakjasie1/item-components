37fbe56c9285aa90717ab092cd4c12f472484028
Lotox <70129125+jakjasie1@users.noreply.github.com>
2026-09-12 11:46:16 +0200

    Update ItemStack_PacketCodecMixin.java for Minecraft 26.2

--- src/main/java/com/boyonk/itemcomponents/mixin/ItemStack_PacketCodecMixin.java
+++ src/main/java/com/boyonk/itemcomponents/mixin/ItemStack_PacketCodecMixin.java
@@ -11,8 +11,8 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(targets = "net.minecraft.item.ItemStack$1")
 public class ItemStack_PacketCodecMixin {
 
-	@ModifyExpressionValue(method = "encode(Lnet/minecraft/network/RegistryByteBuf;Lnet/minecraft/item/ItemStack;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/component/MergedComponentMap;getChanges()Lnet/minecraft/component/ComponentChanges;"))
-	ComponentChanges defaultitemcomponents$sendExtraChanges(ComponentChanges original, @Local(argsOnly = true) ItemStack stack) {
+	@ModifyExpressionValue(method = "encode", at = @At(value = "INVOKE", target = "Lnet/minecraft/component/MergedComponentMap;getChanges()Lnet/minecraft/component/ComponentChanges;"))
+	ComponentChanges itemcomponents$sendExtraChanges(ComponentChanges original, @Local(argsOnly = true) ItemStack stack) {
 		ComponentChanges base = ItemComponents.MANAGER.getChanges(stack.getItem());
 		if (base.isEmpty()) return original;
 		if (original.isEmpty()) return base;
